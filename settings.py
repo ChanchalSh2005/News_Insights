1c8b55c85ec5e4cd8d0bb252aa46619e60e805f0
Chanchal Sharma <chanchalsh931@gmail.com>
2026-07-17 15:25:31 +0530

    Change env_file to NONE in Settings model config

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from sqlalchemy.engine import URL
 
 
 class Settings(BaseSettings):
-    model_config=SettingsConfigDict(env_file=".env",extra="ignore")
+    model_config=SettingsConfigDict(env_file=NONE)
     DB_CONNECTION:str
 
 setting=Settings()
