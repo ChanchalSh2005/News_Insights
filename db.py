981d2f99f344f093850e295561cea93c70f896ee
Chanchal Sharma <chanchalsh931@gmail.com>
2026-07-17 16:34:35 +0530

    Updation

--- db.py
+++ db.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,DeclarativeBase
 from settings import setting
-engine=create_engine(url="mysql+pymysql://root:isVMSfGtBFZHudxsZybUcyuHVhKsazpy@mysql--nzw.railway.internal:3306/railway")
+import os
+user = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+host = os.getenv("MYSQLHOST")
+port = os.getenv("MYSQLPORT")
+database = os.getenv("MYSQLDATABASE")
+DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+
 
 LocalSession=sessionmaker(bind=engine)
 class Base(DeclarativeBase):
