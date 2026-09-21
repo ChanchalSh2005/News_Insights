43faf1ee9df895dd93e01a31215ad1ba2d49d62d
Chanchal Sharma <chanchalsh931@gmail.com>
2026-09-21 21:36:14 +0530

    Enhance database engine configuration
    Added pool_pre_ping option to database engine for better connection handling.

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from settings import setting
 url=setting.DB_CONNECTION
 print("DB_CONNECTION:",url)
 print("repr =", repr(url))
-engine=create_engine(url=url)
+engine=create_engine(url=url, pool_pre_ping=True,)
 
 LocalSession=sessionmaker(bind=engine)
 class Base(DeclarativeBase):
